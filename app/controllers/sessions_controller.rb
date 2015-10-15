fec2c8ed824f9897777be764ed60ce512e4eb232
wangzhou666 <wdx6666666@gmail.com>
2015-10-14 20:58:22 -0500

    cha ge biao

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,6 @@ class SessionsController < ApplicationController
   def create
   	@user = User.find_by(employee_id: params[:session][:employee_id].downcase)
   	if @user && @user.authenticate(params[:session][:password])
-      subodinate_ids = find_subodinate_ids(@user)
-      @subodinates = subodinate_ids.map {|m| UserInfo.find_by(employee_id: m)}
   		redirect_to user_path(@user) and return
   	else
       flash.now[:danger] = 'Invalid password'
