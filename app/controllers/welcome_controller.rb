9384d3413a6b6f52f1921478a8e269a7c399c3e6
wangzhou666 <wdx6666666@gmail.com>
2015-10-30 09:45:45 -0500

    go to signed in page from root if there is user in cookie

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+	def index
+		unless current_user.nil?
+			redirect_to url_for(:controller => :sessions, :action => :index) and return
+		end
+	end
 end
