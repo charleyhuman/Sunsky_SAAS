f2bd2e4f9f761c2baa23da2443dc6384ffbb4732
dayang <jeffxdy@gmail.com>
2015-11-10 14:30:26 -0600

    added two pages

--- db/migrate/20151027224629_create_class_attds.rb
+++ db/migrate/20151027224629_create_class_attds.rb
@@ -2,10 +2,10 @@ class CreateClassAttds < ActiveRecord::Migration
   def change
     create_table :class_attds do |t|
     	t.string :class_number
-		t.string :ssn
-		t.string :employee_id
-		t.string :ce_hours
-		t.string :grade
+  		t.string :ssn
+  		t.string :employee_id
+  		t.string :ce_hours
+  		t.string :grade
       t.timestamps null: false
     end
   end
--- db/migrate/20151028185232_create_certificates.rb
+++ db/migrate/20151028185232_create_certificates.rb
@@ -3,12 +3,11 @@ class CreateCertificates < ActiveRecord::Migration
     create_table :certificates do |t|
 
     	t.string :ssn
-		t.string :employee_id
-		t.string :certificate
-		t.string :issued_by
-		t.string :date_issued
-		t.string :credit_hours
-
+  		t.string :employee_id
+  		t.string :certificate
+  		t.string :issued_by
+  		t.string :date_issued
+  		t.string :credit_hours
       t.timestamps null: false
     end
   end
